b2dba90750e0458617b28f0e0ecefaf8445693ea
Elif-Rana <122517868+Elif-Rana@users.noreply.github.com>
2024-01-30 09:33:44 +0300

    Update centralized.py

--- centralized.py
+++ centralized.py
@@ -49,7 +49,7 @@ def test(net, testloader):
     return loss, accuracy
 
 def load_data():
-    trf = Compose([ToTensor(),Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+    trf = Compose([ToTensor(),Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     # Split the data
     trainset = CIFAR10("./data", train=True, download=True, transform=trf)
